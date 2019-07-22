8a94b4070a6a79864e1c77e51d0fe89ee56a23b3
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-07-22 00:57:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -27,3 +27,20 @@ class Triangle
     end  
   end 
 end
+
+def equilateral?
+    (@side1 == @side2) && (@side1 == @side3)
+  end
+
+  def scalene?
+    (@side1 != @side2) && (@side1!= @side3) && (@side2 != @side3)
+  end
+
+  def isosceles?
+    (@side2 == @side3) || (@side1 == @side2) || (@side1 = @side3)
+  end
+
+  def valid?
+    (@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side1 + @side3 > @side2)
+  end
+end 
