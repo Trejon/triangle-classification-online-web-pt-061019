ced3db044055cb12abb2afe2bfe75c716fb143a0
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-07-22 00:45:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,7 @@
 class Triangle
+ attr_accessor :side_one, :side_two, :side_three
  
- def initialize(side_one_length, side_two_length, side_three_length)
+ def initialize(side_one,side_two,side_three)
    
  end 
  
